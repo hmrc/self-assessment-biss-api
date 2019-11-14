a7e2027319df3a15ecf6aace533b66817c9f6f70
Adrian Jones <adrian@nboaram.co.uk>
2019-11-14 09:13:47 +0000

    MTDSA-4318 Request Parser and Validation

--- test/v1/mocks/validators/MockRetrieveUKPropertyBISSValidator.scala
+++ test/v1/mocks/validators/MockRetrieveUKPropertyBISSValidator.scala
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package v1.mocks.validators
 
 import org.scalamock.handlers.CallHandler1
